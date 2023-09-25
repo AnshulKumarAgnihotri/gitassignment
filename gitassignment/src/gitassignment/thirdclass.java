1ca0b61c100cf3b67f7e2cd5dfa5642e3335ac7c
AnshulAgnihotri <anshulkit1999@gmail.com>
2023-09-25 19:59:07 +0530

    my program second branch

--- gitassignment/src/gitassignment/thirdclass.java
+++ gitassignment/src/gitassignment/thirdclass.java
@@ -2,12 +2,6 @@ package gitassignment;
 
 public class thirdclass {
 	{
-<<<<<<< HEAD
-        System.out.println("thirdclass + change in first branch"); 
-=======
-        System.out.println("thirdclass + change for stash"); 
->>>>>>> 153ab4ba326c86fbedfde564bf7eeede4078d432
+        System.out.println("thirdclass"); 
     }
-
-
 }
